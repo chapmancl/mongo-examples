d160b69cc3ca5c79f755f40446f90d621373a957
Chris Chapman <cchapman@confluent.io>
2026-02-18 14:53:08 -0500

    Update MongoMCP settings for local environment

--- webui/backend/settings.py
+++ webui/backend/settings.py
@@ -1,9 +1,10 @@
 # for hosted dynamic mcp server (MongoMCP)
-mongo_mcp_root = "https://mcp.mongoarena.com"
+mongo_mcp_root = "local"
 
-AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsImFwaV9rZXkiOiJiOTY3MjlmZS1kMTJhLTExZjAtODhkOS0yMDE1ZGUwNDdiYjciLCJ0eXAiOiJKV1QifQ.eyJhZ2VudF9uYW1lIjoiY29uc29sZV9jaGF0Ym90In0.4bxuofjKUyRTI_1bCLTwK4ikbpcS_Bm2U883Wspk8s8"
+AUTH_TOKEN = ""
 
 
 # AWS settings
 aws_region="us-east-2"
+
 LLM_MODEL_ID = "global.anthropic.claude-haiku-4-5-20251001-v1:0"
